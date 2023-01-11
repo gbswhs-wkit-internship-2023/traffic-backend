9860292531cc2bee8fa70951d6f660ff108b3e40
Minhyeok Park <pmh_only@pmh.codes>
2023-01-11 16:28:18 +0900

    fix: unnamed import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
-import express from 'express'
+import * as express from 'express'
 import { join } from 'path'
 
 async function boot (): Promise<void> {
