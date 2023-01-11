7a1cb416744109c7c7d5650fd4b98c52c007d395
Minhyeok Park <pmh_only@pmh.codes>
2023-01-11 16:27:13 +0900

    fix: payload limit to 20mb

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
+import express from 'express'
 import { join } from 'path'
 
 async function boot (): Promise<void> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
+  app.use(express.json({ limit: '20mb' }))
 
   app.useGlobalPipes(new ValidationPipe({
     always: true,
